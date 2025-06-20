5035f06177ef51e64a7cb64889626c642ba86376
oflatt <oflatt@gmail.com>
2025-06-20 15:36:46 -0600

    demo with keyboard input

--- main.gd
+++ main.gd
@@ -1,19 +1,35 @@
 extends Node2D
 
+var square
+var velocity = Vector2.ZERO
+
 func _ready():
 	var vp_size = get_viewport_rect().size
-	var scale_factor = vp_size.y  # 1.0 = height
-
+	var scale_factor = vp_size.y
 	self.scale = Vector2(scale_factor, scale_factor)
-
-	print("Main is running!")
 
 	var image = Image.create(1, 1, false, Image.FORMAT_RGBA8)
 	image.fill(Color.RED)
 	var tex = ImageTexture.create_from_image(image)
 
-	var sprite = Sprite2D.new()
-	sprite.texture = tex
-	sprite.scale = Vector2(0.1, 0.1)  # Size relative to height
-	sprite.position = Vector2(0.4, 0.1)
-	add_child(sprite)
+	square = Sprite2D.new()
+	square.texture = tex
+	square.scale = Vector2(0.1, 0.1)
+	square.position = Vector2(0.4, 0.1)
+	add_child(square)
+
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
+
+	square.position += velocity * 0.01
